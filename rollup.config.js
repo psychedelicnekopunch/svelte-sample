8fecff3053864d99bae4e00cb33849d5a2f9d655
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-05-24 00:34:40 +0900

    update

--- rollup.config.js
+++ rollup.config.js
@@ -20,7 +20,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/bundle.js',
+		file: 'public/js/bundle.js',
 		// globals: {
 		// 	'request': 'Request',
 		// 	'axios': 'Axios',
@@ -33,7 +33,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
-				css.write('public/bundle.css');
+				css.write('public/css/bundle.css');
 			},
 			// Sass
 			preprocess: preprocess({
